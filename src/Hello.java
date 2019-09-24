b564bbe50a7988a1f7c34262f2a88dd556519e60
evboggs302 <evboggs94@gmail.com>
2019-09-24 11:32:03 -0400

    intro to version control with intelliJ

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        System.out.println("Hello Evan, this is your first Java creation.");
+
+        int myFirstNum = 5;
+
+        System.out.println("myFirstNum = " + myFirstNum);
+//         sout + TAB = intelliJ shortcut for System.out.printIn();
+
+
+    }
+}
